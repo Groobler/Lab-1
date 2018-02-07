89afa8f9f3e47fe194c4c3dd6de741343d2fc845
Austin Griebler <31248971+Groobler@users.noreply.github.com>
2018-02-07 08:12:52 -0600

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,275 @@
+var myObject=[
+  {
+    "Player": "Mikael Granlund",
+    "GP": 81,
+    "G": 26,
+    "A": 43,
+    "Pts": 69,
+    "Hits": 50
+ 
+  },
+  {
+    "Player": "Eric Staal",
+    "GP": 82,
+    "G": 28,
+    "A": 37,
+    "Pts": 65,
+    "Hits": 42
+ 
+  },
+  {
+    "Player": "Mikko Koivu",
+    "GP": 80,
+    "G": 18,
+    "A": 40,
+    "Pts": 58,
+    "Hits": 32
+
+  },
+  {
+    "Player": "Nino Niederreiter",
+    "GP": 82,
+    "G": 25,
+    "A": 32,
+    "Pts": 57,
+    "Hits": 120
+ 
+  },
+  {
+    "Player": "Charlie Coyle",
+    "GP": 82,
+    "G": 18,
+    "A": 38,
+    "Pts": 56,
+    "Hits": 101
+ 
+  },
+  {
+    "Player": "Jason Zucker",
+    "GP": 79,
+    "G": 22,
+    "A": 25,
+    "Pts": 47,
+    "Hits": 73
+ 
+  },
+  {
+    "Player": "Jason Pominville",
+    "GP": 78,
+    "G": 13,
+    "A": 34,
+    "Pts": 47,
+    "Hits": 44
+ 
+  },
+  {
+    "Player": "Zach Parise",
+    "GP": 69,
+    "G": 19,
+    "A": 23,
+    "Pts": 42,
+    "Hits": 46
+ 
+  },
+  {
+    "Player": "Ryan Suter",
+    "GP": 82,
+    "G": 9,
+    "A": 31,
+    "Pts": 40,
+    "Hits": 72
+
+ 
+  },
+  {
+    "Player": "Jared Spurgeon",
+    "GP": 76,
+    "G": 10,
+    "A": 28,
+    "Pts": 38,
+    "Hits": 49
+
+ 
+  },
+  {
+    "Player": "Matt Dumba",
+    "GP": 76,
+    "G": 11,
+    "A": 23,
+    "Pts": 34,
+    "Hits": 96
+ 
+  },
+  {
+    "Player": "Erik Haula",
+    "GP": 72,
+    "G": 15,
+    "A": 11,
+    "Pts": 26,
+    "Hits": 39
+ 
+  },
+  {
+    "Player": "Jonas Brodin",
+    "GP": 68,
+    "G": 3,
+    "A": 22,
+    "Pts": 25,
+    "Hits": 20
+ 
+  },
+  {
+    "Player": "Chris Stewart",
+    "GP": 79,
+    "G": 13,
+    "A": 8,
+    "Pts": 21,
+    "Hits": 85
+ 
+  },
+  {
+    "Player": "Martin Hanzal",
+    "GP": 20,
+    "G": 4,
+    "A": 9,
+    "Pts": 13,
+    "Hits": 40
+ 
+  },
+  {
+    "Player": "Marco Scandella",
+    "GP": 71,
+    "G": 4,
+    "A": 9,
+    "Pts": 13,
+    "Hits": 50
+ 
+  },
+  {
+    "Player": "Jordan Schroeder",
+    "GP": 37,
+    "G": 6,
+    "A": 7,
+    "Pts": 13,
+    "Hits": 29
+ 
+  },
+  {
+    "Player": "Tyler Graovac",
+    "GP": 52,
+    "G": 7,
+    "A": 2,
+    "Pts": 9,
+    "Hits": 52
+ 
+  },
+  {
+    "Player": "Christian Folin",
+    "GP": 51,
+    "G": 2,
+    "A": 6,
+    "Pts": 8,
+    "Hits": 69
+ 
+  },
+  {
+    "Player": "Joel Eriksson Ek",
+    "GP": 15,
+    "G": 3,
+    "A": 4,
+    "Pts": 7,
+    "Hits": 23
+  },
+  {
+    "Player": "Nate Prosser",
+    "GP": 39,
+    "G": 2,
+    "A": 5,
+    "Pts": 7,
+    "Hits": 30
+  },
+  {
+    "Player": "Gustav Olofsson",
+    "GP": 13,
+    "G": 0,
+    "A": 3,
+    "Pts": 3,
+    "Hits": 10
+  },
+  {
+    "Player": "Ryan White",
+    "GP": 19,
+    "G": 2,
+    "A": 1,
+    "Pts": 3,
+    "Hits": 49
+ 
+  },
+  {
+    "Player": "Zac Dalpe",
+    "GP": 9,
+    "G": 1,
+    "A": 2,
+    "Pts": 3,
+    "Hits": 5
+ 
+  },
+  {
+    "Player": "Christoph Bertschy",
+    "GP": 5,
+    "G": 0,
+    "A": 1,
+	"Pts": 1,
+    "Hits": 7
+  },
+  {
+    "Player": "Kurtis Gabriel",
+    "GP": 13,
+    "G": 0,
+    "A": 1,
+    "Pts": 1,
+    "Hits": 26
+  },
+  {
+    "Player": "Teemu Pulkkinen",
+    "GP": 9,
+    "G": 1,
+    "A": 0,
+    "Pts": 1,
+    "Hits": 2
+
+  },
+  {
+    "Player": "Mike Reilly",
+    "GP": 17,
+    "G": 1,
+    "A": 0,
+    "Pts": 1,
+    "Hits": 2
+ 
+  },
+  {
+    "Player": "Alex Tuch",
+    "GP": 6,
+    "G": 0,
+    "A": 0,
+    "Pts": 0,
+    "Hits": 5
+  },
+  {
+    "Player": "Patrick Cannone",
+    "GP": 3,
+    "G": 0,
+    "A": 0,
+    "Pts": 0,
+    "Hits": 2
+  },
+  {
+    "Player": "Zack Mitchell",
+    "GP": 11,
+    "G": 0,
+    "A": 0,
+    "Pts": 0,
+    "Hits": 14
+  }
+]
